41164eed4b2f213f084cbeaea748ed64b0062ef2
mincrion <mincrion@google.com>
2024-12-10 23:43:16 +0400

    implement toNumber

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -226,8 +226,9 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  const x = Number(value);
+  return Number.isNaN(x) ? def : x;
 }
 
 /**
