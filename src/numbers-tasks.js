a909ac0939c60ad43ff8123c19bd88c5f523d0d7
mincrion <mincrion@google.com>
2024-12-10 23:38:48 +0400

    implement isPrime

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -200,8 +200,15 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n <= 1) return false;
+  if (n === 2) return true;
+  if (n % 2 === 0) return false;
+  const m = Math.sqrt(n);
+  for (let i = 3; i <= m; i += 2) {
+    if ( n % i === 0) return false;
+  }
+  return true;
 }
 
 /**
